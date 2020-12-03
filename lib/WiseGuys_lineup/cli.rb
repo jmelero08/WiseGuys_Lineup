e556f545e5ee08b116385f8106bab7f0c23c8903
Jose Melero Martinez <jmelero08@gmail.com>
2020-12-03 14:47:02 -0700

    more commands

--- lib/WiseGuys_lineup/cli.rb
+++ lib/WiseGuys_lineup/cli.rb
@@ -1,7 +1,8 @@
 class WiseGuysLineup::CLI
     def call
-        puts 'Welcome to WiseGuys Comedy Club!'
+        puts "\nWelcome to WiseGuys Comedy Club!\n"
         get_locations
+        list_locations
         get_user_location
         #get_comics_for(location)
         #list_comics
@@ -9,12 +10,27 @@ class WiseGuysLineup::CLI
 
     def get_locations
         #can be scrapped instead
-        @locations = ['Ogden', 'Salt_Lake_City', 'Jordan_Landing']
+        @locations = ["Ogden", "Salt Lake City", "Jordan Landing"]
     end
     
-    def get_user_location
-        @locations.each_with_index do |location, index|
-            puts "#{index + 1}. #{location}"
+    def list_locations
+        puts 'Choose location to see lineup.'
+        @locations.each.with_index(1) do |location, index|
+            puts "#{index}. #{location}"
         end 
+    end
+
+    def get_user_location
+        chosen_location = gets.strip.to_i
+        show_lineup_for(chosen_location) if valid_input(chosen_location, @locations)
+    end
+
+    def valid_input(input, data)
+        input.to_i <= data.length && input.to_i > 0 
+    end
+
+    def show_lineup_for(chosen_location)
+        location = @locations[chosen_location - 1]
+        puts "Here are the comics for #{location}"
     end
 end 
