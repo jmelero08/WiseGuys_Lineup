be6b3e8c98a983965304ba6ef1ac037329b52048
Jose Melero Martinez <jmelero08@gmail.com>
2020-12-03 13:26:03 -0700

    location code

--- lib/WiseGuys_lineup/cli.rb
+++ lib/WiseGuys_lineup/cli.rb
@@ -1,5 +1,20 @@
 class WiseGuysLineup::CLI
     def call
         puts 'Welcome to WiseGuys Comedy Club!'
-    end 
+        get_locations
+        get_user_location
+        #get_comics_for(location)
+        #list_comics
+    end
+
+    def get_locations
+        #can be scrapped instead
+        @locations = ['Ogden', 'Salt_Lake_City', 'Jordan_Landing']
+    end
+    
+    def get_user_location
+        @locations.each_with_index do |location, index|
+            puts "#{index + 1}. #{location}"
+        end 
+    end
 end 
